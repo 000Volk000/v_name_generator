089848ccabcb1c44b11d74fbdf5370dbcae058dc
Darío Martínez Kostyuk <dariomartk@gmail.com>
2025-12-08 00:49:04 +0100

    feat: Check for (letter_number / 2) number of vowels

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use rand::{Rng, rngs::ThreadRng};
 
-fn generate_random_name(size: i8, mut rng: ThreadRng) -> String {
+fn generate_random_name(size: i8, rng: &mut ThreadRng) -> String {
     let mut name = String::new();
     name.push('V');
     let letters_size = ('z' as u32) - ('a' as u32);
@@ -12,8 +12,27 @@ fn generate_random_name(size: i8, mut rng: ThreadRng) -> String {
     name
 }
 
+fn how_many_vowels(name: String) -> i8 {
+    let mut n = 0;
+    let vowels = ['a', 'e', 'i', 'o', 'u'];
+
+    for letter in name.chars() {
+        if vowels.contains(&letter) {
+            n += 1;
+        }
+    }
+
+    n
+}
+
 fn main() {
     let mut rng = rand::rng();
     let letter_number = 3 + (rng.random::<i8>() % 3).abs();
-    println!("{}", generate_random_name(letter_number, rng));
+    let mut name = generate_random_name(letter_number, &mut rng);
+
+    while how_many_vowels(name.clone()) < (letter_number / 2) {
+        name = generate_random_name(letter_number, &mut rng);
+    }
+
+    println!("{}", name);
 }
