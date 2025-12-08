263d4564b6544808f8f7c61988e988f71e4f3c65
Darío Martínez Kostyuk <dariomartk@gmail.com>
2025-12-08 14:54:26 +0100

    feat: restructurated code for order

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,14 @@
-use rand::{Rng, rngs::ThreadRng};
+mod name_functions;
+use rand::Rng;
 
-fn generate_random_name(size: i8, rng: &mut ThreadRng) -> String {
-    let mut name = String::new();
-    name.push('V');
-    let letters_size = ('z' as u32) - ('a' as u32);
-
-    for _ in 0..size {
-        name.push(((('a' as u32) + (rng.random::<u32>() % letters_size)) as u8).into());
-    }
-
-    name
-}
-
-fn how_many_vowels(name: String) -> i8 {
-    let mut n = 0;
-    let vowels = ['a', 'e', 'i', 'o', 'u'];
-
-    for letter in name.chars() {
-        if vowels.contains(&letter) {
-            n += 1;
-        }
-    }
-
-    n
-}
+use crate::name_functions::{generate_random_name, is_valid_name};
 
 fn main() {
     let mut rng = rand::rng();
     let letter_number = 3 + (rng.random::<i8>() % 3).abs();
     let mut name = generate_random_name(letter_number, &mut rng);
 
-    while how_many_vowels(name.clone()) < (letter_number / 2) {
+    while !is_valid_name(name.clone()) {
         name = generate_random_name(letter_number, &mut rng);
     }
 
---
+++ src/name_functions.rs
@@ -0,0 +1,34 @@
+use rand::{Rng, rngs::ThreadRng};
+
+fn how_many_vowels(name: String) -> i8 {
+    let mut n = 0;
+    let vowels = ['a', 'e', 'i', 'o', 'u'];
+
+    for letter in name.chars() {
+        if vowels.contains(&letter) {
+            n += 1;
+        }
+    }
+
+    n
+}
+
+pub fn generate_random_name(size: i8, rng: &mut ThreadRng) -> String {
+    let mut name = String::new();
+    name.push('V');
+    let letters_size = ('z' as u32) - ('a' as u32);
+
+    for _ in 0..size {
+        name.push(((('a' as u32) + (rng.random::<u32>() % letters_size)) as u8).into());
+    }
+
+    name
+}
+
+pub fn is_valid_name(name: String) -> bool {
+    if how_many_vowels(name.clone()) < ((name.len() as i8 - 1) / 2) {
+        return false;
+    }
+
+    true
+}
