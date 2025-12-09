2cf2bbbe38f4e5299cea26528533ab92aca3d35a
Darío Martínez Kostyuk <dariomartk@gmail.com>
2025-12-09 18:52:42 +0100

    feat: added async function for http post request to ntfy

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,28 @@
 mod name_functions;
 use dotenv::dotenv;
 use rand::Rng;
+use reqwest::Response;
 use std::env;
 
 use crate::name_functions::{generate_random_name, is_valid_name};
 
-fn main() {
+async fn send_ntfy(name: String) -> Result<Response, reqwest::Error> {
+    dotenv().expect("Couldn load the .env");
+    let ntfy = env::var("NTFY_POST_URL").expect("Couldn't find the NTFY_POST_URL on the .env");
+
+    let response = reqwest::Client::new()
+        .post(ntfy)
+        .body(format!("The new name is: {}", name.to_owned()))
+        .header("Title", "New V-Name Generated")
+        .header("Tags", "rotating_light")
+        .send()
+        .await?;
+
+    Ok(response)
+}
+
+#[tokio::main]
+async fn main() {
     let mut rng = rand::rng();
     let letter_number = 3 + (rng.random::<i8>() % 3).abs();
     let mut name = generate_random_name(letter_number, &mut rng);
@@ -17,10 +34,10 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     if args.len() > 1 && args.get(1).expect("Error geting the 1 arg") == "ntfy" {
-        dotenv().expect("Couldn load the .env");
-        let ntfy = env::var("NTFY_POST_URL").expect("Couldn't find the NTFY_POST_URL on the .env");
-
-        println!("{}", ntfy)
+        match send_ntfy(name).await {
+            Ok(response) => println!("ntfy sent correctly\nResponse code: {}", response.status()),
+            Err(e) => println!("Something wrong happened sending the ntfy\nError: {e}"),
+        }
     } else {
         println!("{}", name);
     }
